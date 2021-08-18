fe871f427e72c7333f3d897ca423410ceaa4ef6a
Meredith Jones <mj19003@gmail.com>
2021-08-18 16:11:24 -0400

    Working on filtering out non-prime numbers

--- primeNumbers.js
+++ primeNumbers.js
@@ -3,14 +3,14 @@ const numArray = document.getElementById('array');
 function sumPrimes(num) {
   //Produce an array containing all number of to and including num
   let numArray = [];
-  for (let i = 1; i <= num; i++) {
+  for (i = 2; i <num; i++) {
     numArray.push(i);
   }
 
   //Remove non-prime numbers from the array     
-  numArray.filter((number) => {
+  numArray.map((number) => {
     for (let i = 2; i < number; i++) {
-        if(number % i === 0) {
+        if(number% i === 0) {
             let index = numArray.indexOf(number);
             return numArray.splice(index, 1);  
             
@@ -24,4 +24,4 @@ function sumPrimes(num) {
 
 }
 
-sumPrimes(10);
+sumPrimes(30);
