29e5d11b3b0b02ac893de720b35651344147de5b
Meredith Jones <mj19003@gmail.com>
2021-08-18 15:47:18 -0400

    Working on finding prime numbers in array

---
+++ primeNumbers.js
@@ -0,0 +1,22 @@
+function sumPrimes(num) {
+  //Produce an array containing all number of to and including num
+  let numArray = [];
+  for (let i = 1; i <= num; i++) {
+    numArray.push(i);
+  }
+
+  //Remove non-prime numbers from the array     
+  numArray.map((number) => {
+    for (let i = 2; i < number; i++) {
+        if(number % i === 0) {
+            let index = numArray.indexOf(number);
+            return numArray.splice(index, 1);       
+        }
+    }   
+  });
+console.log(numArray);
+//  return numArray;
+
+}
+
+sumPrimes(10);
