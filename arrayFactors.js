30ce36e08c0ec56c7a85073bc4de1e6aaf64f3ac
Meredith Jones <mj19003@gmail.com>
2021-08-20 16:59:55 -0400

    Working on while loop

--- arrayFactors.js
+++ arrayFactors.js
@@ -2,3 +2,16 @@ function arrayFactors(number){
     const factors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     dividedBy = 2;
 }
+
+while(number>2) {
+    if (number% dividedBy == 0){
+        factors.push(dividedBy);
+        number = number/dividedBy;
+    }
+
+else{
+    dividedBy++;
+        }
+    }
+    return factors;
+}
